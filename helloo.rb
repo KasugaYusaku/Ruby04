e3c32a9095082f1579819c5d50b810b5f18aa043
春日優作 <yuayanao0105@gmail.com>
2020-03-24 07:51:55 +0000

    Ruby02課題

--- helloo.rb
+++ helloo.rb
@@ -1,3 +1,23 @@
-p "Branding Engineer"
-p "Hello"
-p "HR Tech"
+puts "初めてのRuby"
+puts "Ruby"+"始めました"
+p 3+3
+p 3-3
+p 3*3
+p 3/3
+array =[1,2,3,4] 
+num_array = array
+p num_array
+profile ={name:"春日優作", birthday:19960105, bloodtype:"O"}
+puts profile[:name]
+puts profile[:birthday]
+puts profile[:bloodtype]
+def plus_ruby(a)
+     a+"ruby"
+end
+p plus_ruby("like")
+p plus_ruby("yhee")
+def plus_one(num)
+    num =num+1
+end
+p plus_one(4)
+p plus_one(10)
